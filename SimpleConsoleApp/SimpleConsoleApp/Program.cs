454e14552a9ae34ad6d476ee0a33d2df742ad8cb
s33277 <s33277@pjwstk.edu.pl>
2026-03-10 11:41:27 +0100

    Add GetAverage for task 2

--- SimpleConsoleApp/SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/SimpleConsoleApp/Program.cs
@@ -1,1 +1,3 @@
 ﻿Console.WriteLine("Hello, World!");
+
+StatisticsHelper.DoSomeStatistics();
--- SimpleConsoleApp/SimpleConsoleApp/StatisticsHelper.cs
+++ SimpleConsoleApp/SimpleConsoleApp/StatisticsHelper.cs
@@ -2,8 +2,15 @@
 
 public class StatisticsHelper
 {
-	public StatisticsHelper()
+	public static void GetAverage()
 	{
+		Console.WriteLine("Enter some nums to find the average:");
 
+		string userNumsString = Console.ReadLine();
+		string[] userNums = userNumsString.Split();
+		List<int> nums = new List<int>();
+		foreach (string n in userNums) nums.Add(int.Parse(n));
+		int average = (int)nums.Average();
+		Console.WriteLine("Average = " + average);
 	}
 }
