262968ab4147b944384afd25c3c1b2423c83b4ec
s33277 <s33277@pjwstk.edu.pl>
2026-03-16 13:15:21 +0100

    Put codes under the same namespace

--- SimpleConsoleApp/SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/SimpleConsoleApp/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿namespace SimpleConsoleApp
+{
+    using System;
 
-Console.WriteLine("Enter some nums to find the average:");
-string userNumsString = Console.ReadLine();
-string[] userNums = userNumsString.Split();
+    Console.WriteLine("Enter some nums to find the average:");
+    string userNumsString = Console.ReadLine();
+    string[] userNums = userNumsString.Split();
 
-Console.WriteLine("Average: "+StatisticsHelper.GetAverage(userNums));
+    Console.WriteLine("Average: "+StatisticsHelper.GetAverage(userNums));
+}
--- SimpleConsoleApp/SimpleConsoleApp/StatisticsHelper.cs
+++ SimpleConsoleApp/SimpleConsoleApp/StatisticsHelper.cs
@@ -1,9 +1,15 @@
-﻿public class StatisticsHelper
+﻿namespace SimpleConsoleApp
 {
-	public static double GetAverage(string[] userNums)
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public class StatisticsHelper
 	{
-		List<double> nums = new List<double>();
-		foreach (string n in userNums) nums.Add(double.Parse(n));
-		return (double)nums.Average();
+		public static double GetAverage(string[] userNums)
+		{
+			List<double> nums = new List<double>();
+			foreach (string n in userNums) nums.Add(double.Parse(n));
+			return nums.Average();
+		}
 	}
 }
