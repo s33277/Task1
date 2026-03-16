1a254f99ff05633df404fe92d4a777ae795eb010
s33277 <s33277@pjwstk.edu.pl>
2026-03-16 13:02:05 +0100

    Change GetAverage return type to double

--- SimpleConsoleApp/SimpleConsoleApp/StatisticsHelper.cs
+++ SimpleConsoleApp/SimpleConsoleApp/StatisticsHelper.cs
@@ -2,14 +2,14 @@
 
 public class StatisticsHelper
 {
-	public static int GetAverage()
+	public static double GetAverage()
 	{
 		Console.WriteLine("Enter some nums to find the average:");
 
 		string userNumsString = Console.ReadLine();
 		string[] userNums = userNumsString.Split();
-		List<int> nums = new List<int>();
-		foreach (string n in userNums) nums.Add(int.Parse(n));
-		return (int)nums.Average();
+		List<double> nums = new List<double>();
+		foreach (string n in userNums) nums.Add(double.Parse(n));
+		return (double)nums.Average();
 	}
 }
