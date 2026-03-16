f213a44893b27ae25de03011844d7c12c0e7d20f
s33277 <s33277@pjwstk.edu.pl>
2026-03-16 19:33:35 +0100

    Rename GetAverage to CalculateAverage

--- SimpleConsoleApp/SimpleConsoleApp/StatisticsHelper.cs
+++ SimpleConsoleApp/SimpleConsoleApp/StatisticsHelper.cs
@@ -5,7 +5,7 @@
 
 	public class StatisticsHelper
 	{
-		public static double GetAverage(string[] userNums)
+		public static double CalculateAverage(string[] userNums)
 		{
 			List<double> nums = new List<double>();
 			foreach (string n in userNums) nums.Add(double.Parse(n));
